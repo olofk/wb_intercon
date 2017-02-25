ea62127b23740117088761cca6f906ae90eed2b6
Olof Kindgren <olof.kindgren@gmail.com>
2017-02-25 22:36:33 +0100

    Hook up wb_cdc_tb to wb_intercon_tb

--- bench/wb_intercon_tb.v
+++ bench/wb_intercon_tb.v
@@ -1,16 +1,19 @@
 module wb_intercon_tb;
 
    vlog_tb_utils vlog_tb_utils0();
-   vlog_tap_generator #("wb_intercon.tap", 2) vtg();
+   vlog_tap_generator #("wb_intercon.tap", 3) vtg();
 
    wb_mux_tb     #(.AUTORUN (0)) wb_mux_tb();
    wb_arbiter_tb #(.AUTORUN (0)) wb_arb_tb();
+   wb_cdc_tb     #(.AUTORUN (0)) wb_cdc_tb();
 
    initial begin
       wb_mux_tb.run;
       vtg.ok("wb_mux: All tests passed!");
       wb_arb_tb.run;
       vtg.ok("wb_arbiter: All tests passed!");
+      wb_cdc_tb.run;
+      vtg.ok("wb_cdc: All tests passed!");
 
       #3 $finish;
    end
